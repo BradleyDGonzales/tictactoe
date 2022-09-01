3b4874a3ee331d9437952325e3d110095ec0c244
Bradley <gonzales.bradleyd@gmail.com>
2022-08-31 23:53:48 -0400

    Add vs. AI and slider toggle

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 /*
 
 
-TODO OPTIONAL: versus AI MACHINE LEARNING (robot)
+TODO OPTIONAL: make the AI unbeatable;
 
 
 */
 const Gameboard = (() => {
-    const winningBoard = [ [0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+    const winningBoard = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     let playerOneArray = [];
     let playerTwoArray = [];
     const board =
-            [``, ``, ``,
-             ``, ``, ``,
-             ``, ``, ``];
+        [``, ``, ``,
+            ``, ``, ``,
+            ``, ``, ``];
     const makeBoard = () => {
         const boardElement = document.getElementById(`board`);
         for (let i = 0; i < board.length; i++) {
@@ -31,22 +31,35 @@ const Gameboard = (() => {
         Gameboard.makeBoard();
         Gameboard.playerOneArray = [];
         Gameboard.playerTwoArray = [];
+        player1Count = 0;
+        player2Count = 0;
         merged = [];
         turn = true;
         match = false;
         tie = false;
         winner = ``;
+        if (document.getElementById(`checkbox1`).checked === true) {
+            document.getElementById(`checkbox1`).checked = false;
+        }
+        if (document.getElementById(`checkbox1`).disabled === true) {
+            document.getElementById(`checkbox1`).disabled = false;
+        }
+        if (hidden === true) {
+            displaySlider();
+        }
         const selectH2Element = document.getElementById(`winner`);
         selectH2Element.innerText = ``;
     }
     const displayWinner = () => {
         const selectH2Element = document.getElementById(`winner`)
+        player1Count = 0;
         if (winner === `` || undefined) {
-            console.log(`draw`);
             selectH2Element.innerText = `Draw! No one wins this one.`;
+            document.getElementById(`checkbox1`).checked = false;
         }
         else {
             selectH2Element.innerText = `${winner} is the winner!`
+            document.getElementById(`checkbox1`).checked = false;
         }
         toggle();
     }
@@ -64,7 +77,7 @@ const Player = (sign) => {
         }
     }
     const playerMove = () => {
-        document.addEventListener(`click`, function(e) {
+        document.addEventListener(`click`, function (e) {
             if (match) {
                 return;
             }
@@ -78,17 +91,64 @@ const Player = (sign) => {
                     let cellID = e.target.getAttribute(`id`).slice(-1);
                     Gameboard.playerOneArray.push(parseInt(cellID));
                     Gameboard.playerOneArray.sort();
-                    findCommonElements(Gameboard.winningBoard,Gameboard.playerOneArray);
+                    findCommonElements(Gameboard.winningBoard, Gameboard.playerOneArray);
+                    player1Count++;
+                    if (player1Count === 1 && document.getElementById(`checkbox1`).checked === false) {
+                        hideSlider();
+                    }
                     if (match) {
                         winner = Player1.mySign();
-                        console.log(`p1 wins`)
                         Gameboard.displayWinner();
                         return;
                     }
-                    
+                    turn = !turn;
+                }
+                if (document.getElementById(`checkbox1`).checked === true) {
+                    document.getElementById(`checkbox1`).disabled = true;
+                    if (player1Count === 5 && !match) {
+                        tie = true;
+                        Gameboard.displayWinner();
+                        return;
+                    }
+                    let randomNumber = Math.floor(Math.random() * 8) + 1;
+                    let randomElement = document.getElementById(`cell${randomNumber}`);
+                    if (randomElement.innerText === `` && !turn) {
+                        randomElement.innerText = Player2.mySign();
+                        turn = !turn;
+                        Gameboard.playerTwoArray.push(parseInt(randomNumber));
+                        Gameboard.playerTwoArray.sort();
+                        findCommonElements(Gameboard.winningBoard, Gameboard.playerTwoArray);
+                        player2Count++;
+                        if (match) {
+                            winner = Player2.mySign();
+                            Gameboard.displayWinner();
+                            return;
+                        }
+                        return;
+                    }
+                    else {
+                        if (player1Count === 5 && !match) {
+                            tie = true;
+                            Gameboard.displayWinner()
+                            document.getElementById(`checkbox1`).checked = false;
+                            return;
+                        }
+                        else {
+                            while (count !== 10) {
+                                randomElement.click();
+                                count++;
+                            }
+                        }
+                        count = 0;
+                    }
                 }
                 else {
-                    sign = 'O';;
+                    if (player1Count === 5 && player2Count === 4 && !match) {
+                        tie = true;
+                        Gameboard.displayWinner();
+                        return;
+                    }
+                    sign = 'O';
                     if (e.target.innerText !== `` || undefined || null) {
                         return;
                     }
@@ -96,21 +156,21 @@ const Player = (sign) => {
                     let cellID = e.target.getAttribute(`id`).slice(-1);
                     Gameboard.playerTwoArray.push(parseInt(cellID));
                     Gameboard.playerTwoArray.sort();
-                    findCommonElements(Gameboard.winningBoard,Gameboard.playerTwoArray);
+                    findCommonElements(Gameboard.winningBoard, Gameboard.playerTwoArray);
+                    player2Count++;
                     if (match) {
                         winner = Player2.mySign();
-                        console.log(`p2 wins`)
                         Gameboard.displayWinner();
                         return;
                     }
+                    turn = !turn;
                 }
                 if (tie) {
                     Gameboard.displayWinner();
                 }
-                turn = !turn;
             }
         })
-        
+
     }
     return { sign, mySign, playerMove };
 }
@@ -121,8 +181,12 @@ let tie;
 let turn = true;
 let merged;
 let winner = ``;
+let count = 0;
+let player1Count = 0;
+let player2Count = 0;
+let hidden;
 Gameboard.makeBoard();
-function findCommonElements (arr1,arr2) {
+function findCommonElements(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         merged = [].concat.apply([], arr1[i]);
         let filteredArray = arr2.filter(value => merged.includes(value));
@@ -140,5 +204,25 @@ function toggle() {
     let popup = document.getElementById(`popup`);
     popup.classList.toggle(`active`);
 }
+function hideSlider() {
+    hidden = true;
+    const slider = document.getElementById(`switch-slider`)
+    const sliderText = document.getElementById(`switch-slider-text`)
+    slider.style.opacity = 0;
+    slider.style.visibility = "hidden";
+    sliderText.style.opacity = 0;
+    sliderText.style.visibility = "hidden";
+}
+function displaySlider() {
+    hidden = false;
+    const slider = document.getElementById(`switch-slider`)
+    const sliderText = document.getElementById(`switch-slider-text`)
+    slider.style.opacity = 100;
+    slider.style.visibility = "visible";
+    sliderText.style.opacity = 100;
+    sliderText.style.visibility = "visible";
+
+}
+
 
 window.onload = Player1.playerMove();
